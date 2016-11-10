d9302d3a015bba3b1c2356ce2c1f7f1b8ad977e3
Csongor <szcsongor2013@gmail.com>
2016-11-10 21:47:59 +0200

    3

--- src/SimpleAgenda.java
+++ src/SimpleAgenda.java
@@ -6,20 +6,23 @@ public class SimpleAgenda {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         int option=0;
-
         do {
 
             // afisare meniu
-            System.out.println("1.fa aia");
-            System.out.println("1.fa ailalta");
+            System.out.println("1.Display Names");
+            System.out.println("2.Add Name");
+            System.out.println("3.Delete Name");
+            System.out.println("4.Exit");
 
 
             //selectare optiune
             option=SkeletonJava.readIntConsole("Option:");
 
             switch (option) {
-                case 1: add();break;
-                case 2: print();break;
+                case 1: print();break;
+                case 2: add();break;
+                case 3: del();break;
+
             }
 
 
@@ -33,20 +36,20 @@ public class SimpleAgenda {
         // de optimizat cand se umple agenda
 
         // de optimizat daca deja exista o pers cu acel nume
-
-        if(index<listName.length) {
-            String name = SkeletonJava.readStringConsole("Input the name:");
+        if (index < listName.length) {
+            String name = SkeletonJava.readStringConsole("Add Name:");
             listName[index] = name;
             index++;
-        }
-        else
-        {
-            System.out.println("gata memoria, schimba telul ");
+        } else {
+            System.out.println("Memory full, please delete");
         }
     }
 
-    static void print() {
 
+    static void print() {
+        for(index=0; index<listName.length; index++) {
+            System.out.println(listName[index]);
+        }
     }
 
     static void del() {
@@ -54,15 +57,30 @@ public class SimpleAgenda {
         // citeste un nume
         // il cauta in array
         //daca il gaseste il sterge  asa listName[unde l-a gasit]=null sau "";
+        SkeletonJava.readIntConsole("Add Name");
+        for(SkeletonJava.readIntConsole("Add Name");
+        if ("AddName".equals(listName[index])) {
+            listName[index] = "";
+
+
+        }
 
     }
-
     static void modify() {
 
         // citeste un nume
         // il cauta in array
         // daca il gaseste cere noul nume si il substituie pe cel vechi cu cel nou
+        SkeletonJava.readIntConsole("Add Name");
+        for(SkeletonJava.readIntConsole("Add Name"){
+            if("Add Name".equals(listName[index])){
+                System.out.println("Please enter new Name: ");
+                listName[index]=SkeletonJava.readIntConsole();
+            }
+
+        }
     }
 
 
 }
+
